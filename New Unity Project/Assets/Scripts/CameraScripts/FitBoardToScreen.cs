9be3f21665dd5b10a448824c4f7572e8e9e56fef
Jesper Turesson <jturesson90@gmail.com>
2016-12-05 17:07:59 +0100

    Android 13

--- New Unity Project/Assets/Scripts/CameraScripts/FitBoardToScreen.cs
+++ New Unity Project/Assets/Scripts/CameraScripts/FitBoardToScreen.cs
@@ -3,17 +3,15 @@ using System.Collections;
 
 public class FitBoardToScreen : MonoBehaviour
 {
-	public float minWidthToBeSeen = 8f;
-    public float MaxWidthToBeSeen = 9f;
-	// Use this for initialization
-	void Start ()
-	{
-   
-        if ((float)Screen.width / (float)Screen.height > 0.7f) {
-            minWidthToBeSeen = MaxWidthToBeSeen;
-        }
-        Camera.main.orthographicSize = minWidthToBeSeen * Screen.height / Screen.width * 0.5f;
+  public float minWidthToBeSeen = 8f;
+  public float MaxWidthToBeSeen = 9f;
+  // Use this for initialization
+  void Start()
+  {
+    if ((float)Screen.width / (float)Screen.height > 0.7f)
+    {
+      minWidthToBeSeen = MaxWidthToBeSeen;
     }
-	
-
+    Camera.main.orthographicSize = minWidthToBeSeen * Screen.height / Screen.width * 0.5f;
+  }
 }
